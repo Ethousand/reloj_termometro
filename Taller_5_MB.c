54e703dadbc75fba23a64d389210e4f1e133d4a2
Emil Colina <cybre333@gmail.com>
2021-03-30 17:03:26 -0400

    inicio de main e inicio de la pantalla lcd

--- Taller_5_MB.c
+++ Taller_5_MB.c
@@ -5,3 +5,23 @@
 #fuses xt, NOPROTECT, NOWDT, NOBROWNOUT, PUT, NOLVP
 
 #include <lcd.c>
+
+// variables de reloj
+int hora, min, seg;
+
+//variable de panel lcd
+
+int fila_1[2] = {1, 1};
+int fila_2[2] = {1, 2};
+
+void main()
+{
+    lcd_init();
+
+    while (true)
+    {
+        lcd_putc('\f');
+        lcd_gotoxy(fila_1);
+        printf(lcd_putc, "hola khe hace");
+    }
+}
