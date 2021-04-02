355e8cb39f9773c964640212a4f333cfa61c89c5
Emil Colina <cybre333@gmail.com>
2021-04-01 23:05:44 -0400

    agregado función para configurar la hora de inicio

--- Taller_5_MB.c
+++ Taller_5_MB.c
@@ -7,12 +7,79 @@
 #include <lcd.c>
 
 // variables de reloj
-int hora, min, seg;
+int hora = 1, min, seg, xm = 0;
+int hora_i, min_i, seg_i;
 
-//variable de panel lcd
+//variables de calendario
+int day, month, year;
+int day_i, month_i, year_i;
 
-int fila_1[2] = {1, 1};
-int fila_2[2] = {1, 2};
+// variables del termometro
+
+//ejecucin del programa
+
+void config_hour()
+{
+    lcd_putc('\f');
+    printf(lcd_putc, "config");
+    delay_ms(500);
+    while (true)
+    {
+        lcd_putc('\f');
+        lcd_gotoxy(1, 1);
+        printf(lcd_putc, "hora:");
+        lcd_gotoxy(1, 2);
+
+        if (xm == 0)
+        {
+            printf(lcd_putc, "%2d:%02d:%02d am", hora_i, min_i, seg_i);
+        }
+        else
+        {
+            printf(lcd_putc, "%2d:%02d:%02d pm", hora_i, min_i, seg_i);
+        }
+
+        Delay_ms(200);
+        switch (porta)
+        {
+        case 0x01:
+            hora_i++;
+            if (hora_i > 12)
+            {
+                hora_i = 1;
+            }
+
+            break;
+        case 0x02:
+            min_i++;
+            if (min_i >= 60)
+            {
+                min_i = 0;
+            }
+
+            break;
+        case 0x04:
+            seg_i++;
+            if (seg_i >= 60)
+            {
+                seg_i = 0;
+            }
+
+            break;
+        case 0x08:
+            xm = 1 - xm;
+            break;
+
+        default:
+            Delay_ms(200);
+            break;
+        }
+    }
+
+    lcd_putc("\f");
+    lcd_gotoxy(1, 1);
+    printf(lcd_putc, "config");
+}
 
 void main()
 {
@@ -21,7 +88,10 @@ void main()
     while (true)
     {
         lcd_putc('\f');
-        lcd_gotoxy(fila_1);
-        printf(lcd_putc, "hola khe hace");
+        lcd_gotoxy(1, 1);
+        printf(lcd_putc, "inicia reloj");
+        delay_ms(500);
+        config_hour();
+        delay_ms(500);
     }
 }
